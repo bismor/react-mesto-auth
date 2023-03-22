129e63f3e6015fdedaeadc5dbad2d247aa28248d
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-03-22 16:05:24 +0300

    удаление пустых тегов

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,56 +1,54 @@
 import PopupWithForm from "./PopupWithForm";
 import React from "react";
 
-function AddPlacePopup ({isOpen, onClose, onAddPlace}) {
-  const inputNameCard = React.useRef()
-  const inputLinkCard = React.useRef()
+function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
+  const inputNameCard = React.useRef();
+  const inputLinkCard = React.useRef();
 
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault();
     onAddPlace({
       name: inputNameCard.current.value,
-      link: inputLinkCard.current.value
-    })
-    onClose()
+      link: inputLinkCard.current.value,
+    });
+    onClose();
   }
 
-  return(
+  return (
     <PopupWithForm
-        title="Новое место"
-        name="cardPopup"
-        isOpen={isOpen}
-        onClose={onClose}
-        buttonText="Сохранить"
-        onSubmit={handleSubmit}
-      >
-        <>
-          <section className="popup__section">
-            <input
-              ref={inputNameCard}
-              type="text"
-              name="name"
-              className="popup__text popup__name"
-              placeholder="Название"
-              required
-              minLength={2}
-              maxLength={30}
-            />
-            <span className="popup__input-error"></span>
-          </section>
-          <section className="popup__section">
-            <input
-              ref={inputLinkCard}
-              type="url"
-              name="about"
-              className="popup__text popup__job"
-              placeholder="Ссылка на картинку"
-              required
-            />
-            <span className="popup__input-error"></span>
-          </section>
-        </>
+      title="Новое место"
+      name="cardPopup"
+      isOpen={isOpen}
+      onClose={onClose}
+      buttonText="Сохранить"
+      onSubmit={handleSubmit}
+    >
+      <section className="popup__section">
+        <input
+          ref={inputNameCard}
+          type="text"
+          name="name"
+          className="popup__text popup__name"
+          placeholder="Название"
+          required
+          minLength={2}
+          maxLength={30}
+        />
+        <span className="popup__input-error"></span>
+      </section>
+      <section className="popup__section">
+        <input
+          ref={inputLinkCard}
+          type="url"
+          name="about"
+          className="popup__text popup__job"
+          placeholder="Ссылка на картинку"
+          required
+        />
+        <span className="popup__input-error"></span>
+      </section>
     </PopupWithForm>
-  )
+  );
 }
 
-export default AddPlacePopup
+export default AddPlacePopup;
